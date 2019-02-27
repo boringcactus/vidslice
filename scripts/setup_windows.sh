4b33474f210650c033eba42364b08fa8fd21dc4e
Melody Horn <melody@boringcactus.com>
2019-02-26 23:43:31 -0700

    why is this so hard

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -7,5 +7,7 @@ echo $PATH
 export PATH=/C/Python37:$PATH
 which python
 python get-pip.py
-curl https://download.lfd.uci.edu/pythonlibs/u2hcgva4/cx_Freeze-5.1.1-cp37-cp37m-win_amd64.whl -o cx_Freeze.whl
-python -m pip install cx_Freeze.whl
+curl https://download.lfd.uci.edu/pythonlibs/u2hcgva4/cx_Freeze-5.1.1-cp37-cp37m-win_amd64.whl -o cx_Freeze-5.1.1-cp37-cp37m-win_amd64.whl
+python -m pip install *.whl
+
+set +x
