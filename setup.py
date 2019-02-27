c2c2d56b4abda215ec60032e3f6e598f36804083
Melody Horn <melody@boringcactus.com>
2019-02-27 11:07:50 -0700

    fix ssl on windows, because of course that doesn't work by default

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+import os
+import sys
+
 from cx_Freeze import setup, Executable
+
+from vidslice import VERSION
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(packages=[], excludes=[])
 
-import sys
-
-from vidslice import VERSION
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+if sys.platform == "win32":
+    buildOptions['include_files'] = [
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'libcrypto-1_1-x64.dll'),
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'libssl-1_1-x64.dll'),
+    ]
 
 base = 'Win32GUI' if sys.platform == 'win32' else None
 
