f5a1ae4e9956f8a120f0a2048ff29953bf032d2d
Melody Horn <melody@boringcactus.com>
2019-02-26 22:44:14 -0700

    further adventures in debugging an environment i don't control

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -2,4 +2,4 @@
 set -x
 
 sudo apt-get update
-sudo apt-get install -y python3.5 python3-pip libgtk-3-dev libgstreamer1.0-dev libsdl2-dev
+sudo apt-get install -y python3.5 python3.5-dev python3-pip libgtk2.0-dev libgtk-3-dev libjpeg-dev libtiff-dev libsdl1.2-dev libgstreamer-plugins-base0.10-dev libnotify-dev freeglut3 freeglut3-dev libsm-dev libwebkitgtk-dev libwebkitgtk-3.0-dev
