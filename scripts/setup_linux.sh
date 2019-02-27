6715b3e7c2db387913264a6ae7059bc0efb0cb0d
Melody Horn <melody@boringcactus.com>
2019-02-26 23:51:17 -0700

    try a different ubuntu version

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -2,7 +2,7 @@
 set -x
 
 sudo apt-get update
-sudo apt-get install -y python3.5 python3.5-dev python3-pip libgtk2.0-dev libgtk-3-dev libjpeg-dev libtiff-dev libsdl1.2-dev libgstreamer-plugins-base0.10-dev libnotify-dev freeglut3 freeglut3-dev libsm-dev libwebkitgtk-dev libwebkitgtk-3.0-dev
-sudo python3 -m pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-14.04/ wxPython==4.0.4
+sudo apt-get install -y python3 python3-dev python3-pip libgtk2.0-dev libgtk-3-dev libjpeg-dev libtiff-dev libsdl1.2-dev libgstreamer-plugins-base0.10-dev libnotify-dev freeglut3 freeglut3-dev libsm-dev libwebkitgtk-dev libwebkitgtk-3.0-dev
+sudo python3 -m pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04/ wxPython==4.0.4
 
 set +x
