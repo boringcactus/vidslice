39cd6b2f45078c960d54ebc06462f24aa2639ea3
Melody Horn <melody@boringcactus.com>
2020-12-24 05:27:33 -0700

    it's always something

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -3,9 +3,9 @@ set -x
 
 choco install python --version 3.7.9 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-echo $PATH
 export PATH=/C/Python37:$PATH
 which python
 python get-pip.py
+ls /C/Python37/DLLs
 
 set +x
