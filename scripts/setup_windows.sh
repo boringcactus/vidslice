eaac8801dedbd063637b15b227fb2d13fb21e075
Melody Horn <melody@boringcactus.com>
2020-12-24 05:40:40 -0700

    correctly disable sh -x

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-set -x
+set +x
 
 choco install python --version 3.7.9 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 export PATH=/C/Python37:$PATH
-which python
 python get-pip.py
-ls /C/Python37/DLLs
 
-set +x
+set -x
