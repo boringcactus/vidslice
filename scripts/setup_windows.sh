5ed17b1867027bc2ccf20ceca77b278042366a33
Melody Horn <melody@boringcactus.com>
2020-12-24 05:01:01 -0700

    get ready to wrestle with build infra for three hours

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-choco install python
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-echo $PATH
-export PATH=/C/Python37:$PATH
-which python
-python get-pip.py
-python -m pip install --upgrade git+https://github.com/anthony-tuininga/cx_Freeze.git@master
-
 set +x
