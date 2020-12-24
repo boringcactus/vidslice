b78e4f4fdfe353835d0fc95eb7ff866ea4b6e2c3
Melody Horn <melody@boringcactus.com>
2020-12-24 05:23:26 -0700

    juggle more dependency stuff

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-sudo apt-get update
-sudo apt-get install -y python3 python3-pip python3-setuptools
-
 set +x
--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-choco install python
+choco install python --version 3.7.9 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 echo $PATH
 export PATH=/C/Python37:$PATH
