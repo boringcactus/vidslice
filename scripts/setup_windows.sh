1f9422539670bbfbcea2083c457dc8075402da1d
Melody Horn <melody@boringcactus.com>
2020-12-24 05:46:38 -0700

    bdg voice i am going to throw myself into the sea

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-set +x
-
 choco install python --version 3.7.9 -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 export PATH=/C/Python37:$PATH
 python get-pip.py
-
-set -x
